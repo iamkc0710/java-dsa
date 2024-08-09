762dea037eaee1bd66bc67ac9ed2aa8e32a8f446
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-09 19:19:26 +0530

    1919

---
+++ JavaBasic 1/Pattern1.java
@@ -0,0 +1,14 @@
+/**
+ * Pattern1
+ * (*****
+ *  ****
+ *  ***
+ *  **
+ *  * )
+ */
+public class Pattern1 {
+
+    public static void main(String[] args) {
+        System.err.println("Hello");
+    }
+}
