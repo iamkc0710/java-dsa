b09e726e5784a3058fa02aa37cf8202ca2c7f8f9
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-11 13:13:50 +0530

    1313

--- JavaBasics-1/JavaInput.java
+++ JavaBasics-1/JavaInput.java
@@ -1,10 +1,7 @@
 public class JavaInput {
     public static void main(String[] args){
-<<<<<<< HEAD
          //Code
-=======
-         //code
->>>>>>> bb04be56268a54605bc80b7d7168f673bff6b82b
+    
     }
 }
 
