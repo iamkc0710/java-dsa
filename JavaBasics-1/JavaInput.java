7235dd8a35f20c10ba869deff3af47ca3a970d24
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-11 13:04:14 +0530

    1304

---
+++ JavaBasics-1/JavaInput.java
@@ -0,0 +1,5 @@
+public class JavaInput {
+    public static void main(String[] args){
+         
+    }
+}
--- JavaBasics-1/JavaOutput.java
+++ JavaBasics-1/JavaOutput.java
@@ -1,6 +1,8 @@
 public class JavaOutput {
     public static void main(String args[]) {
         System.out.println("Hello World");  // Next Line
+        System.out.print("Hello World \n");  // Another method for next line
         System.out.print("Hello World");  // Same Line
+
     }
 }
