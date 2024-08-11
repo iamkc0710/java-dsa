255c6bd406c260f74db33b36e3a6682af7d0c321
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-11 12:03:43 +0530

    1204

--- JavaBasics-1/BoilerplateCode.java
+++ JavaBasics-1/BoilerplateCode.java
@@ -1,7 +1,8 @@
-
 public class BoilerplateCode {
-
     public static void main(String[] args) {
-       
+    //    Code 
     }
 }       
+
+// This is Boilerplate Code of java
+
---
+++ JavaBasics-1/JavaOutput.java
@@ -0,0 +1,6 @@
+public class JavaOutput {
+    public static void main(String args[]) {
+        System.out.println("Hello World");  // Next Line
+        System.out.print("Hello World");  // Same Line
+    }
+}
