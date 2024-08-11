32ace67c6363e061b30ad5910bd667b244e9b71c
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-11 20:09:22 +0530

    2010

---
+++ JavaBasics-1/JavaVariables.java
@@ -0,0 +1,20 @@
+public class JavaVariables {
+    public static void main(String[] args) {
+      int a = 10;
+      int b;
+      
+      float c = 2.2f;
+      String d = "Karan";
+
+      char e = 'C';
+      double f = 20.5;
+
+      System.out.println(a);
+      // System.out.println(b);
+      System.out.println(c);
+      System.out.println(d);
+      System.out.println(e);
+      System.out.println(f);
+
+    }
+}
