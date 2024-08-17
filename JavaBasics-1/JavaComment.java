33226597d0d7f5c50bba71697365bee139d9ff3f
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-17 13:54:18 +0530

    1356

---
+++ JavaBasics-1/JavaComment.java
@@ -0,0 +1,10 @@
+public class JavaComment {
+    public static void main(String[] args) {
+        // This is one line comment.
+
+        /*
+          This is multi line 
+          comment.
+         */
+    }
+}
--- JavaBasics-1/JavaInput.java
+++ JavaBasics-1/JavaInput.java
@@ -1,8 +1,44 @@
+/*
+  Input In Java :
+
+  next
+  nextLine
+  nextint
+  nextByte
+  nextFloat
+  nextDouble
+  nextBoolean
+  nextShort
+  nextLong
+                  
+ */
+
+import java.util.Scanner;
+
 public class JavaInput {
     public static void main(String[] args){
          
+    // Take Input from user in java
+
+        Scanner sc = new Scanner(System.in);
+        // String input = sc.next();
+        // System.out.println(input);
+
+        // String name = sc.nextLine();
+        // System.out.println(name);
+
+        // int num = sc.nextInt();
+        // System.out.println(num);
+
+        // float f = sc.nextFloat();
+        // System.out.println(f);
+
+        // double d = sc.nextDouble();
+        // System.out.println(d);
+
+        boolean b = sc.nextBoolean();
+        System.out.println(b);
 
     }
 }
 
-// Take Input from user in java
