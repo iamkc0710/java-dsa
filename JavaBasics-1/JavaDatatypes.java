7c4811e23056ec997517ebf3b84560b029c1e037
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-17 12:37:03 +0530

    1236

---
+++ JavaBasics-1/JavaDatatypes.java
@@ -0,0 +1,20 @@
+public class JavaDatatypes {
+    public static void main(String args[]){
+        byte b = 8;
+        System.out.println(b);
+
+        char ch = 'a';
+        System.out.println(ch);
+
+
+        boolean var = false;
+        float price = 1.5f;
+        int number = 25;
+
+        long lg = 50000;
+
+        System.out.println(lg);
+
+
+    } 
+}
---
+++ JavaBasics-1/Sumoftwo.java
@@ -0,0 +1,12 @@
+public class Sumoftwo {
+    public static void main(String[] args) {
+        // Using third variable
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+
+        System.out.println(sum);
+        System.out.println(a + b);       // Without Using third variable
+
+    }
+}
