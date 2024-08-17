c84f87687f5eabb4671fc64485c18871c78ca8b9
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-17 20:47:53 +0530

    1236

---
+++ JavaBasics-1/TypeCasting.java
@@ -0,0 +1,15 @@
+public class TypeCasting{
+    public static void main(String[] args) {
+
+        // Implicit Casting
+
+        // int numOne = 15;
+        // float numTwo = numOne;
+
+        // Explicit Casting
+        float numOne = 15.5f;
+        int numTwo = (int) numOne;
+
+        System.out.println(numTwo);
+    }
+}
