5c54b56d8e1e2355a70eaaea593ebe1461f815a8
thechaurasiya <thekaranchaurasiya@gmail.com>
2024-08-21 11:22:19 +0530

    1122

---
+++ JavaOperators/JavaArithmetic.java
@@ -0,0 +1,26 @@
+package JavaOperators;
+
+public class JavaArithmetic {
+    public static void main(String args[]){
+
+    // Binary Arithmetic Operators
+    int a = 10;
+    int b = 5;
+    
+    System.out.println(a+b);
+    System.out.println(a-b);
+    System.out.println(a*b);
+    System.out.println(a/b);
+    System.out.println(a%b);
+
+    
+    // Unary Arithmetic Operators
+    a++;
+    b--;  // b = b-1
+
+    System.out.println(a);
+    System.out.println(b);
+
+
+    }
+}
---
+++ JavaOperators/JavaRelational.java
@@ -0,0 +1,15 @@
+package JavaOperators;
+
+public class JavaRelational {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 15;
+
+        System.out.println(a==b);
+        System.out.println(a!=b);
+        System.out.println(a>b);
+        System.out.println(a<b);
+        System.out.println(a>=b);
+        System.out.println(a<=b);
+    }
+}
